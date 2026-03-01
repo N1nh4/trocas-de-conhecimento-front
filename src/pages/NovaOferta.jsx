00b9e5ba68e397f7847ceda058f406f131f4e624
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2026-03-01 11:47:34 -0300

    estruta básica para cadastro de novas ofertas

--- src/pages/NovaOferta.jsx
+++ src/pages/NovaOferta.jsx
@@ -1,8 +1,155 @@
+"use client";
+import { useEffect, useState } from "react";
+import { criarConhecimentoService } from "../services/conhecimentoService";
+
 export default function NovaOferta() {
-    return (
-        <div>
-            <h1>Cadastro de Oferta</h1>
-            {/* Formulário de cadastro */}
+  const [titulo, setTitulo] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [nivel, setNivel] = useState("");
+  const [responsavel, setResponsavel] = useState("");
+  const [pessoas, setPessoas] = useState([]);
+
+  useEffect(() => {
+    async function pessoaService() {
+      const resp = await fetch("http://localhost:3000/pessoas/");
+      const data = await resp.json();
+      setPessoas(data);
+    }
+    pessoaService();
+  }, []);
+
+  const categorias = [
+    "TECNOLOGIA",
+    "DESIGN",
+    "IDIOMAS",
+    "MUSICA",
+    "CULINARIA",
+    "ARTES",
+    "MARKETING",
+    "FINANCAS",
+    "SAUDE",
+    "EDUCACAO",
+    "ESPORTES",
+    "OUTROS",
+  ];
+
+  const niveis = ["INICIANTE", "INTERMEDIARIO", "AVANCADO"];
+
+  async function handleSubmit(e) {
+    const novaOferta = {
+      titulo,
+      descricao,
+      categoria,
+      nivel,
+      pessoaId: Number(responsavel),
+    };
+    e.preventDefault();
+    await criarConhecimentoService(novaOferta);
+  }
+
+  return (
+    <div className="flex flex-col items-center bg-[#F9FBFA] h-screen ">
+      <div className="flex flex-col items-start justify-start mt-10">
+        <div className="flex flex-col mb-8">
+          <h1 className="font-bold text-3xl">Nova Oferta de Conhecimento</h1>
+          <p>Compartilhe o que você sabe com a comunidade.</p>
         </div>
-    );
+        {/* Formulário de cadastro */}
+        <div className="bg-white text-black flex flex-col font-semibold p-6 rounded-xl w-md ">
+          <form
+            action=""
+            className="flex flex-col gap-4 "
+            onSubmit={handleSubmit}
+          >
+            <div className="flex flex-col">
+              <label htmlFor="">Título *</label>
+              <input
+                type="text"
+                name="titulo"
+                id="titulo"
+                placeholder="Ex: React do Zero"
+                value={titulo}
+                onChange={(e) => setTitulo(e.target.value)}
+                className="bg-[#F9FBFA] border border-[#DCE5E0] rounded-xl  py-2 px-3"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="">Descrição *</label>
+              <textarea
+                name="descricao"
+                id="descriacao"
+                placeholder="Descreva o conhecimento..."
+                value={descricao}
+                onChange={(e) => setDescricao(e.target.value)}
+                className="bg-[#F9FBFA] border border-[#DCE5E0] rounded-xl py-2 px-3"
+              ></textarea>
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="">Categoria *</label>
+              <select
+                name="categoria"
+                id="categoria"
+                onChange={(e) => setCategoria(e.target.value)}
+                value={categoria}
+                className="bg-[#F9FBFA] border border-[#DCE5E0] rounded-xl py-2 px-3"
+              >
+                <option value="">Selecione</option>
+                {categorias.map((c) => (
+                  <option key={c} value={c}>
+                    {c}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="">Nível *</label>
+              <select
+                name="nivel"
+                id="nivel"
+                onChange={(e) => setNivel(e.target.value)}
+                value={nivel}
+                className="bg-[#F9FBFA] border border-[#DCE5E0] rounded-xl  py-2 px-3"
+              >
+                <option value="">Selecione</option>
+                {niveis.map((n) => (
+                  <option key={n} value={n}>
+                    {n}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="">Pessoa responsável *</label>
+              <select
+                name="responsavel"
+                id="responsavel"
+                onChange={(e) => setResponsavel(e.target.value)}
+                value={responsavel}
+                className="bg-[#F9FBFA] border border-[#DCE5E0] rounded-xl  py-2 px-3"
+              >
+                <option value="">Selecione</option>
+                {pessoas.map((p) => (
+                  <option key={p.id} value={p.id}>
+                    {p.nome}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex flex-rol w-full justify-between gap-2">
+              <button
+                className="bg-green-800 w-full py-2 px-4 rounded-xl text-white cursor-pointer"
+                type="submit"
+              >
+                Salvar
+              </button>
+              <button className="bg-[#F9FBFA] w-full py-2 px-4 rounded-xl cursor-pointer">
+                Cancelar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/services/conhecimentoService.js
+++ src/services/conhecimentoService.js
@@ -1,33 +1,51 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+  baseURL: "http://localhost:3000",
 });
 
 // Função para listar conhecimentos com filtros (Rhobertta)
 export async function listConhecimentos(filters = {}) {
-    try {
-        const response = await api.get('/conhecimentos', {
-            params: filters
-        });
+  try {
+    const response = await api.get("/conhecimentos", {
+      params: filters,
+    });
 
-        return response.data;
-
-    } catch (error) {
-        if (error.response) {  // erro no backend
-            throw new Error(
-                error.response.data?.message || 'Erro ao listar conhecimentos'
-            );
-        } else {
-            throw new Error('Erro de conexão com o servidor');
-        }
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      // erro no backend
+      throw new Error(
+        error.response.data?.message || "Erro ao listar conhecimentos",
+      );
+    } else {
+      throw new Error("Erro de conexão com o servidor");
     }
+  }
 }
 
 // Função para criar um novo conhecimento (Alana)
+
+async function criarConhecimentoService(data) {
+  try {
+    const response = await fetch("http://localhost:3000/conhecimentos/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      console.error("Erro ao criar Conhecimento");
+    }
+    return response.json();
+  } catch (error) {
+    throw new Error("Erro ao criar Conhecimento");
+  }
+}
 
 // Função para buscar um conhecimento por ID para exibir detalhes (Alana)
 
 // Função para atualizar um conhecimento existente (Alana)
 
 // Função para excluir um conhecimento (Alana)
+
+export { criarConhecimentoService };
